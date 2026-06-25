a84c5048b219f222ec8ba920b403f5f27129eb97
Nick-Kevin <nicblou23@gmail.com>
2026-06-25 14:36:58 +0400

    first commit

---
+++ keep-awake.py
@@ -0,0 +1,46 @@
+"""
+    This script is used visit each project I've deployed every 1 hour to prevent them from sleep. 
+"""
+
+import time
+import requests
+from datetime import datetime
+
+VISIT_INTERVAL = 3600 # 1 hour
+
+urls = [
+    "https://nick-kevin-track-popularity.hf.space/",
+    "https://nick-kevin-credit-scoring-ai.hf.space/",
+]
+
+# add a User-Agent header to your request
+# Streamlit's servers block or endlessly redirect basic automated scripts
+# that use Python's default urllib user agent string to protect themselves against bot 
+
+# Real browser headers
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+
+while True:
+    now = datetime.now()
+    print("\n\nVisit at:", now)
+
+    for url in urls:
+        try:
+            with requests.Session() as session:
+                session.headers.update(HEADERS)
+
+                # allow_redirects=True explicitly tells Python to follow Streamlit's routing paths
+                response = session.get(url, timeout=20, allow_redirects=True)
+                
+                print("------------------------------------")
+                print(f"Ping successful! Final URL reached: {response.url}")
+                print(f"Response Status Code: {response.status_code}")
+        except ValueError:
+            print(f"Ping {url} failed: the website is sleeping")
+            print(f"Error: {ValueError}")
+
+    time.sleep(VISIT_INTERVAL)
